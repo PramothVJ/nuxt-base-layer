502057dc65e8142aec4028924b231888ce21c326
Pramoth Kumar <166225216+PramothVJ@users.noreply.github.com>
2024-10-22 15:37:05 +0530

    Update SomeComponent.vue

--- components/SomeComponent.vue
+++ components/SomeComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h1>1 changed Some component from "base" layer!</h1>
-    <h1>1 changed Some component from "base" layer!</h1>
+    <h1>1 1111changed Some component from "base" layer!</h1>
   </div>
 </template>
